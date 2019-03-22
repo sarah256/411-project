57f975d2e3bff8691d0aefd339e55968d9cb00b8
srieger <srieger256@gmail.com>
2019-03-22 15:10:08 -0400

    Twitter API call template

---
+++ TwitterClient.py
@@ -0,0 +1,87 @@
+import re
+import nltk
+import tweepy 
+from tweepy import OAuthHandler 
+
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+
+class TwitterClient(object): 
+	''' 
+	Generic Twitter Class for analysis. 
+	'''
+	def __init__(self): 
+		''' 
+		Class constructor or initialization method. 
+		'''
+		# keys and tokens from the Twitter Dev Console 
+		consumer_key = ''
+		consumer_secret = ''
+		access_token = ''
+		access_token_secret = ''
+
+		# attempt authentication 
+		try: 
+			# create OAuthHandler object 
+			self.auth = OAuthHandler(consumer_key, consumer_secret) 
+			# set access token and secret 
+			self.auth.set_access_token(access_token, access_token_secret) 
+			# create tweepy API object to fetch tweets 
+			self.api = tweepy.API(self.auth) 
+		except: 
+			print("Error: Authentication Failed") 
+
+	def clean_tweet(self, tweet): 
+		''' 
+		Utility function to clean tweet text by removing links, special characters 
+		using simple regex statements. 
+		'''
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split()) 
+
+	def get_tweets(self, query, count): 
+		''' 
+		Main function to fetch tweets and parse them.
+		Returns an array of arrays of tokenized tweets.
+		'''
+		# empty list to store parsed tweets 
+		tweets = [] 
+
+		try: 
+			# call twitter api to fetch tweets 
+			fetched_tweets = self.api.search(q = query, count = count) 
+
+			# parsing tweets one by one 
+            for tweet in fetched_tweets: 
+                # empty dictionary to store required params of a tweet 
+                parsed_tweet = {} 
+  
+                # saving text of tweet 
+                parsed_tweet['text'] = tweet.text 
+                # saving sentiment of tweet 
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text) 
+  
+                # appending parsed tweet to tweets list 
+                if tweet.retweet_count > 0: 
+                    # if tweet has retweets, ensure that it is appended only once 
+                    if parsed_tweet not in tweets: 
+                        tweets.append(parsed_tweet) 
+                else: 
+                    tweets.append(parsed_tweet) 
+
+			# return parsed and tokenized tweets 
+			return tweets 
+
+		except tweepy.TweepError as e: 
+			# print error (if any) 
+			print("Error : " + str(e)) 
+
+def main(): 
+	# creating object of TwitterClient Class 
+	api = TwitterClient() 
+	# calling function to get tweets 
+	# tweets = api.get_tweets(query = 'slang', count = 1000) 
+
+
+if __name__ == "__main__": 
+	# calling main function 
+	main() 
---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+from TwitterClient import TwitterClient
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+@app.route("/input", methods=["POST"])
+def input_text():
+
+	if request.method == "POST":
+		print(request.form)
+		data = request.form['submitText']
+		print(data)
+		query = data
+		# creating object of TwitterClient Class 
+		api = TwitterClient() 
+		# calling function to get tweets 
+		tweets = api.get_tweets(query = query, count = 100)
+
+	return render_template("api_prototype.html", tweets=tweets) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
