f2ed221dc3f84fb5ffab6bef183eda8cb93e7eab
srieger <srieger256@gmail.com>
2019-04-25 19:48:39 -0400

    Add movies API

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from TwitterClient import TwitterClient
+import requests
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
@@ -8,6 +9,14 @@ def home():
 
 @app.route("/input", methods=["POST"])
 def input_text():
+	#NOTE - make sure to put API key into url
+	url = "https://api.themoviedb.org/3/discover/movie?api_key=XXX&language=en-US&sort_by=popularity.desc&with_genres=28"
+	r = requests.get(url)
+	full_json = r.json()
+	movies = {} # key: movie name ; value: sentiment
+	# print(full_json)
+	for movie in full_json['results']:
+		movies[movie['title']] = []
 
 	if request.method == "POST":
 		print(request.form)
@@ -16,11 +25,18 @@ def input_text():
 		query = data
 		# creating object of TwitterClient Class 
 		api = TwitterClient() 
-		# calling function to get tweets 
-		tweets = api.get_tweets(query = query, count = 100)
-		sentiment = api.get_sentiment(tweets)
+		# calling function to get tweets
+		count = 0
+		final_movies = {}
+		for movie in movies: 
+			tweets = api.get_tweets(query = movie, count = 100)
+			sentiment = api.get_sentiment(tweets)
+			final_movies[movie] = sentiment
+			count = count + 1
+			if count == 5:
+				break
 
-	return render_template("home.html", tweets=sentiment) 
+	return render_template("home.html", movies=final_movies) 
 
 if __name__ == "__main__":
     app.run(debug=True)
